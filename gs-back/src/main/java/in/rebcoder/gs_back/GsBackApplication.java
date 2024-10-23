c63765175bce338540ee012c83d994806a8048b3
kartik <kartik31193@gmail.com>
2024-10-24 02:51:50 +0530

    master: initial commit

---
+++ gs-back/src/main/java/in/rebcoder/gs_back/GsBackApplication.java
@@ -0,0 +1,13 @@
+package in.rebcoder.gs_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsBackApplication.class, args);
+	}
+
+}
---
+++ gs-back/src/test/java/in/rebcoder/gs_back/GsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package in.rebcoder.gs_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
